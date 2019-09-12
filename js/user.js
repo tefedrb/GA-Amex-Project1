2e1a17d464dfba48960a7da33beecb49fb73d1d6
tefedrb-enterprise <tefebell@gmail.com>
2019-09-12 11:51:59 -0400

    transfered functionality from main.js to user.js

--- js/user.js
+++ js/user.js
@@ -1,12 +1,4 @@
 // comment icon to hide and reveal upon click within post
-function revealComment() {
-  const commentBtn = document.querySelector('.cmntBtn');
-  if (commentBtn.style.display === "none") {
-    commentBtn.style.display = "block";
-  } else {
-    commentBtn.style.display = "none";
-  }
-}
 const userNameDOM = document.querySelector('#user');
 const classIn = document.querySelector('.email');
 const passwordIn = document.querySelector('.password');
@@ -19,9 +11,31 @@ const signUpBtn = document.querySelector('.signUpBtn');
 const signUpForm = document.querySelector('.signUpForm');
 const logInForm = document.querySelector('.logInForm');
 
-console.log("hello")
+function revealComment(){
+  const commentBtn = document.querySelector('.cmntBtn');
+  if (commentBtn.style.display === "none") {
+    commentBtn.style.display = "block";
+  } else {
+    commentBtn.style.display = "none";
+  }
+}
 
+const updateProfile = () => {
+  const userProfile = JSON.parse(localStorage.userProfile)
+  // console.log(userProfile);
+  if(loginToken){
+    document.querySelector('#innerUser').innerText =
+    userProfile.user.username;
+    document.querySelector('#backUpEmail').innerText =
+    userProfile.additionalEmail;
+    document.querySelector('#mobile').innerText =
+    userProfile.mobile;
+    document.querySelector('#address').innerText =
+    userProfile.address;
+  }
+};
 
+// updateProfile();
 const switchPages = () => {
   const urlArry = window.location.href.split('/');
   const newUrl = urlArry.slice(0,urlArry.length-1)
@@ -32,14 +46,6 @@ const switchPages = () => {
     location.replace(newUrl + '/index.html');
   }
 };
-
-const testUser1 = {
-  email: "TestThis10@someplace.com",
-  password: "123452239",
-  username: "HipHopJoe9"
-}
-
-let userToken = null;
 
 const newUser = (email, pass, user) => {
   fetch('http://thesi.generalassemb.ly:8080/signup', {
@@ -99,13 +105,6 @@ const existingUser = (email, pass) => {
   })
 };
 
-const logIn = (event) => {
-  event.preventDefault();
-  const email = event.target[0].value;
-  const pass = event.target[0].value;
-  existingUser(email, pass);
-};
-
 const createProfile = (event) => {
   event.preventDefault();
   const backupEmail = event.target[0].value;
@@ -159,11 +158,9 @@ function updateDom(){
   .catch((err) => {
       console.log(err);
   })
-}
-}
-​
+};
 
-const createPost(event) {
+const createPost = (event) => {
   event.preventDefault();
   const title = document.querySelector('.title');
   const description = document.querySelector('.description');
@@ -187,28 +184,7 @@ const createPost(event) {
   .catch((err) => {
       console.log(err);
   })
-}
-
-// const listAllPosts = () => {
-//   fetch('http://thesi.generalassemb.ly:8080/post/list', {
-//     method: 'GET',
-//     headers: {
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({
-//       additionalEmail: additionalEmail,
-//       mobile: mobile,
-//       address: address
-//     })
-//   })
-//   .then(response => response.json())
-//   .then(response => {
-//     console.log('List All Posts', response);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   })
-// };
+};
 
 const deleteComment = () => {
   fetch('http://thesi.generalassemb.ly:8080/post/list', {
@@ -231,37 +207,10 @@ const deleteComment = () => {
   })
 };
 
-const userPage = () => {
-
-};
-
 settings.addEventListener('click', function(e){
   if(dropDownMenu.classList.contains('create-profile-slide')){
     dropDownMenu.classList.remove('create-profile-slide');
   } else {
     dropDownMenu.classList.add('create-profile-slide');
   }
-});
-
-setUserForm.addEventListener('mouseover', function(e){
-    if(e.target === signUpBtn){
-      for(let i = 0; i < 3; i++){
-        signUpForm.children[i].classList.add('showInputs');
-        signUpForm.children[i].classList.remove('collapseInputs');
-      }
-      for(let i = 0; i < 2; i++){
-        logInForm.children[i].classList.remove('showInputs');
-        logInForm.children[i].classList.add('collapseInputs');
-      }
-    }
-    if(e.target === logInBtn){
-      for(let i = 0; i < 3; i++){
-        signUpForm.children[i].classList.remove('showInputs');
-        signUpForm.children[i].classList.add('collapseInputs');
-      }
-      for(let i = 0; i < 2; i++){
-        logInForm.children[i].classList.add('showInputs');
-        logInForm.children[i].classList.remove('collapseInputs');
-      }
-    }
 });
